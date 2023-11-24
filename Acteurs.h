99f010106e8a314b58f9ade2c40ca88e18120ff6
Abdou Samatte Diop <abdou-samatte.diop@uha.fr>
2023-11-24 17:11:38 +0100

    Ajout de la classe de base

--- Acteurs.h
+++ Acteurs.h
@@ -1,6 +1,8 @@
 #ifndef ACTEURS_H_INCLUDED
 #define ACTEURS_H_INCLUDED
+class acteurs
+{
 
-
+};
 
 #endif // ACTEURS_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+
     return 0;
 }
