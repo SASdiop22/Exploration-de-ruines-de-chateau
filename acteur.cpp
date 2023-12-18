3911d8698cb8e46222661d196600a97f20ad1ac3
Abdou Samatte Diop <abdou-samatte.diop@uha.fr>
2023-12-18 19:23:08 +0100

    Modification de la classe acteur

--- acteur.cpp
+++ acteur.cpp
@@ -1,41 +1,175 @@
 #include "acteur.h"
  #include <random>
 
-aventurier::aventurier(double  vie, double force,double bourse, double stabiliteEpee, double stabiliteArmure ):d_pvie{vie}, d_pforce{force},
+/***********************************   AVENTURIER    *************/
+
+aventurier::aventurier(double  vie, double force,double bourse, double stabiliteEpee, double stabiliteArmure ): d_pvie{vie}, d_pforce{force},
 d_bourse{bourse}, d_Epee{stabiliteEpee}, d_Armure{stabiliteArmure}
 {}
 
 double aventurier:: point_vie() const
 { return d_pvie;
 }
+
+void aventurier::PointVie(double pVie)
+{
+    d_pvie= pVie;
+}
+
 double aventurier::point_force() const
 {
     return d_pforce;
 }
 
-double  aventurier:: ForceAttaque()
+double aventurier::bourse() const
+{
+    return d_bourse;
+}
+
+double  aventurier:: Attaque_Avec_Force()
 {  double r {d_pforce+ d_Epee.solidite()};
    double proba = std::rand() % 2;
    if(proba<0.8)
     return r*0.9;
 }
 
-void aventurier::EstAttaquee( double force)
+void aventurier::Tue_Monstre(acteur& Monstre)
 {
-   d_Armure-= force*(3/4); 
-   d_pvie-= d_pvie*(1/4);
+    d_pforce += Monstre.point_force()/4;
+    d_pvie+=(Monstre.point_force()*3)/4;
 }
+bool aventurier::estMort() const
+{
+     return d_pvie == 0.0;
+
+}
+
+double aventurier::x() const
+{
+  return d_x;
+}
+
+double aventurier::y() const
+{  return d_y;
+}
+
+void  aventurier::x (double x)
+{
+    d_x = x;
+}
+void  aventurier::y(double y)
+{
+    d_y= y;
+}
+void aventurier::symboleActeur() const
+{
+        std::cout << "A";
+}
+
+ double aventurier::SoliditeEpee() const
+ {
+     return d_Epee.solidite();
+ }
+
+    void aventurier::pSoliditeEpee(double p)
+    {
+        d_Epee.Pointsolidite(p);
+    }
+//void aventurier::EstAttaque( double force)
+//{
+//   d_Armure.pointsSoldite()-= force*(3/8);
+//   d_pvie-= d_pvie*(1/4);
+//}
+
+void  aventurier::attaque(acteur& act)
+{
+    double f= this->Attaque_Avec_Force();
+    double point = d_Epee.solidite();
+    point --;
+    d_Epee.Pointsolidite(point);
+
+    double p= act.point_vie();
+    p-= f;
+    act.PointVie(p);
+}
+
+
+
 /** deplacement d'un aventurier on a dit �a � gerer dans l'interface */
-void aventurier::SeDeplace()
+//void aventurier::SeDeplace()
+//{
+//}
+
+
+/************************** MONSTRE *****************************/
+
+
+monstre::monstre(double pointVie,double pointForce, double habilite ): d_pvie {pointVie}, d_pforce{pointForce}, d_habilite{habilite}
 {}
+
 double monstre::habilite() const
 {
-
+   return d_habilite;
 }
 
-void monstreVoyant:: SeDeplace()
+ double monstre:: point_vie() const
+ {
+     return d_pvie;
+ }
+
+ void monstre::PointVie(double pVie)
+ {
+     d_pvie= pVie;
+ }
+
+double monstre::point_force() const
 {
+    return d_pforce;
+}
 
+double  monstre::Attaque_Avec_Force()
+{
+    return d_pforce*0.9* d_habilite;
+
+}
+ bool  monstre::estMort() const
+ {
+     return d_pvie == 0.0;
+ }
+
+//void monstre::attaque(acteur& act)
+//{
+//  double f= this->Attaque_Avec_Force();
+//  double ps=
+//}
+void monstre::attaque(aventurier& a)
+{     double f= this->Attaque_Avec_Force();
+       double p= a.SoliditeEpee();
+       p-=(f*3)/8;
+        a.pSoliditeEpee(p);
+        double r= a.point_vie();
+        r-= f/4;
+        a.PointVie(r);
 }
 
 
+ double monstre::x() const
+ {
+     return d_x;
+ }
+ double monstre::y() const
+ {
+     return d_y;
+ }
+void monstre::x (double x)
+{
+      d_x= x;
+}
+void monstre::y(double y)
+{
+    d_y= y;
+}
+void monstre::symboleActeur() const
+{
+      std::cout << "M";
+}
--- acteur.h
+++ acteur.h
@@ -4,56 +4,102 @@
 #include<iostream>
 #include <ostream>
 
+#include "armes.h"
+
+
 class acteur
 {
 public:
     virtual ~acteur()= default;
-    virtual double point_vie()const = 0;
-    virtual double point_force()const=0;
-    virtual void ForceAttaque()=0;
-    virtual void EstAttaque(double force )=0;
-    virtual void SeDeplace()=0;
-    /**position x et y comme methode */
+    virtual double point_vie()const;
+    virtual double point_force()const;
+   virtual void PointVie(double pVie) =0;
+    virtual double   Attaque_Avec_Force() =0;
+//    virtual void EstAttaque(double force )=0;
+//    /**reste a voir*/
+    virtual void attaque( acteur& act) =0;
+//    virtual void SeDeplace()=0;
+     virtual double x() const=0;
+     virtual  double y() const=0;
+    virtual void symboleActeur() const=0;
+     virtual void x (double x)=0;
+     virtual void y(double y)=0;
+
 };
 
 
+/**************************** AVENTURIER *****************************/
 
-class aventurier:public acteur
+class aventurier :public acteur
 {
 public:
     aventurier(double vie, double force,double bourse, double stabiliteEpee, double stabiliteArmure );
     double point_vie() const override;
+    void PointVie(double pVie) override;
+
     double point_force() const override;
-    double  ForceAttaque() override;
-    void EstAttaquee( double force ) override;
-    void SeDeplace() override;
+    double bourse() const;
+    double Attaque_Avec_Force() override;
+    void Tue_Monstre (acteur& Monstre);
+    bool estMort() const;
+    double SoliditeEpee() const;
+    void pSoliditeEpee(double p);
+    //void EstAttaque( double force ) override;
+     /**reste a voir*/
+//    void SeDeplace() override;
+     void attaque(acteur& act) override;
+    /** */
+    double x() const override;
+    double y() const override;
+    void x (double x) override;
+    void y(double y) override;
+    void symboleActeur() const override;
 private:
- double  d_pvie, d_pforce;
+
+ double d_pvie, d_pforce, d_x, d_y;
  double d_bourse;
- epee d_Epee;
- armure d_Armure;
+ armes d_Epee;
+ armes d_Armure;
 };
+
+
+ /**   ***********************    MONSTRE   ********************/
 
 class monstre:public acteur
 {public:
     monstre(double pointVie,double pointForce, double habilite );
     double habilite() const ;
     double point_vie() const override;
+    void PointVie(double pVie) override;
     double point_force() const override;
+    double  Attaque_Avec_Force() override;
+     bool estMort() const;
+    // void attaque(acteur& act) override;
+    void attaque(aventurier& a);
+
+      /**reste a voir*/
+//    void SeDeplace() override;
+//    void ataque(acteur& M) override;
+    double x() const override;
+    double y() const override;
+    void x (double x) override;
+    void y(double y) override;
+    void symboleActeur() const override;
+
 private:
-    double d_pvie, d_pforce;
+   double d_pvie, d_pforce, d_x, d_y;
     double d_habilite;
 
 };
 
 class monstreVoyant:public monstre
 {public:
-    void SeDeplace() override;
+ //    void SeDeplace() override;
 
 };
 class monstreAveugle: public monstre
 {public:
-    void SeDeplace() override;
+   // void SeDeplace() override;
 };
 
 #endif // ACTEUR_H_INCLUDED
