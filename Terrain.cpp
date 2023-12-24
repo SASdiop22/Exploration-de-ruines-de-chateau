3168153736cef6f5d1e0d3c26fc693101509ee7c
Gadeu Vinette <gadeumarcy@gmail.com>
2023-12-24 11:24:44 +0100

    ajout de la méthide qui vérifie si la valeur aléatoire généreée existe déjà ça permettra de ne pas reéter les cases non accessibles

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,6 +1,28 @@
 #include "Terrain.h"
+#include"monstre.h"
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+using std::cout;
+using std::endl;
 
 
+bool isAmongValue(std::vector<int>setOfRandomValue, int randomValue)
+{
+    int i = 0;
+    while (i < setOfRandomValue.size())
+    {
+        if (setOfRandomValue[i] != randomValue)
+        {
+            i++;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    return false;
+}
 terrain::terrain(const std::vector<std::vector<char>>& tableau):d_tableau{tableau}
 {
 
@@ -53,6 +75,52 @@ void terrain::attribuerMurTerrain()
 
         }
     }
+    for(int i = 1; i< hauteur()-1; i+=2)
 
+    {
+         std::vector<int>setOfRandomValue;
+        setOfRandomValue.push_back(0);
+        int caseInacessible =  static_cast<int>(hauteur()/2);
+        cout<<'c'<<caseInacessible<<endl;
+
+
+
+        while( setOfRandomValue.size()!= (caseInacessible+1))
+        {
+            int randomValue = (std::rand() % (hauteur() - 2)) + 1;
+
+            if(isAmongValue(setOfRandomValue,randomValue)== false)
+            {
+
+                setOfRandomValue.push_back(randomValue);
+                 d_tableau[randomValue][i] =  '#';
+            }
+
+        }
+        cout<<"dans la boucle for"<<endl;
+
+
+    }
+    cout<<"Hors"<<endl;
+
+
+}
+void terrain::initialiserActeur(aventurier& aventurier,std::vector<std::unique_ptr<monstre>>& monstres)
+{
+    d_tableau[aventurier.position().x()][aventurier.position().y()] = aventurier.symbole();
+
+    for(auto &m: monstres)
+    {
+        int randomValueLigne = (std::rand() % (largeur() - 2)) + 1;
+        cout<<randomValueLigne;
+        int randomValueColonne = (std::rand() % (hauteur() - 2)) + 1;
+        cout<<randomValueColonne;
+        if(d_tableau[randomValueLigne][randomValueColonne] != '#')
+        {
+           d_tableau[randomValueLigne][randomValueColonne] = m->symbole();
+        }
+
+
+    }
 
 }
