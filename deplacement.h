85886fb39d04cb63f1cc273f4dbec7c96e6a67b5
Gadeu Vinette <gadeumarcy@gmail.com>
2023-12-27 20:43:24 +0100

    solution trouvée. On garde les pointeurs intelligents

--- deplacement.h
+++ deplacement.h
@@ -1,18 +1,19 @@
 #ifndef DEPLACEMENT_H_INCLUDED
 #define DEPLACEMENT_H_INCLUDED
 #include "acteur.h"
+#include"memory"
 class deplacement
 {
 public:
     deplacement();
-     void deplaceRight(acteur* A);
-    void deplaceLeft(acteur* A);
-    void deplaceUp(acteur* A);
-    void deplaceDown(acteur* A);
-    void deplaceRightUp(acteur* A);
-    void deplaceRightDown(acteur* A);
-    void deplaceLeftUp(acteur* A);
-    void deplaceLeftDown(acteur* A);
+     void deplaceRight(std::unique_ptr<acteur> A);
+    void deplaceLeft(std::unique_ptr<acteur> A);
+    void deplaceUp(std::unique_ptr<acteur> A);
+    void deplaceDown(std::unique_ptr<acteur> A);
+    void deplaceRightUp(std::unique_ptr<acteur> A);
+    void deplaceRightDown(std::unique_ptr<acteur> A);
+    void deplaceLeftUp(std::unique_ptr<acteur> A);
+    void deplaceLeftDown(std::unique_ptr<acteur> A);
 
 };
 
