3b0076f2ab61454b0ac4e5930f77e26e6c266823
NFSTOURE <151944692+NFSTOURE@users.noreply.github.com>
2024-01-01 14:56:27 +0100

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,11 +1,11 @@
-#include "game.h"
-game::game(): d_aventurier{}, d_monstres{}, d_terrain{}
+#include"game.h"
+#include"point.h"
+#include"deplacement.h"
+
+/*game::game(): d_aventurier{nullptr}, d_monstres{}, d_terrain{}
 {
 }
-terrain game::terrainGame() const
-{
-    return d_terrain;
-}
+
 
 void game::ajoute(std::unique_ptr<aventurier> A)
 {
@@ -18,41 +18,89 @@ void game::ajoute(std::unique_ptr<monstre> M)
     d_monstres.push_back(std::move(M));
 }
 
-void game::ajoute(terrain T)
+
+void game::remplirTerrain(int l, int h)
 {
-    d_terrain = T;
-}
-void game::remplirTerrain()
-{
+    d_terrain = terrain(l,h);
     d_terrain.attribuerMurTerrain();
     d_terrain.initialiserActeur(d_aventurier,d_monstres);
 }
 
 
-void game::combat(std::unique_ptr<aventurier>& A, std::unique_ptr<monstre>& M)
+void game::combat()
 {
-    while(bool tour = true)
-    {
-        M->attaque(A);
-        if(A->estMort())
+    //if(d_monstres[0]->position() == d_aventurier->position())
+    //{
+        while(bool tour = true)
         {
-            tour = false;
-        }
-        else
-        {
-            A->attaque(M);
-            if(M->estMort())
+            d_monstres[0]->attaque(d_aventurier);
+            if(d_aventurier->estMort())
             {
-                int i {0};
-                while(i < d_monstres.size() && d_monstres[i] != M)
-                {
-                    i++;
-                }
-                d_monstres.erase(d_monstres.begin()+i);
                 tour = false;
             }
+            else
+            {
+                d_aventurier->attaque(d_monstres[0]);
+                if(d_monstres[0]->estMort())
+                    d_monstres
+                    tour = false;
+            }
         }
+    //}
+}*/
+
+void game::deplaceAventurier(int choix)
+{
+    deplacement d{};
+    switch (choix) {
+    case 1 ://se deplacer à droit
+        if( d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()] != '#')
+
+            d.deplaceRight(d_aventurier);
+        break;
+    case 2 ://se deplacer à gauche
+        if( d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()] != '#')
+
+            d.deplaceLeft(d_aventurier);
+        break;
+    case 3 ://se deplacer en haut
+         if( d_terrain.tableau()[d_aventurier->position().x()][d_aventurier->position().y()-1] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()][d_aventurier->position().y()-1] != '#')
+
+            d.deplaceUp(d_aventurier);
+         break;
+    case 4 :// se deplacer en bas
+        if( d_terrain.tableau()[d_aventurier->position().x()][d_aventurier->position().y()+1] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()][d_aventurier->position().y()+1] != '#')
+
+            d.deplaceDown(d_aventurier);
+        break;
+    case 5 : //se deplacer en haut vers la gauche 'lu'
+        if( d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()-1] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()-1] != '#')
+
+            d.deplaceLeftUp(d_aventurier);
+        break;
+    case 6 ://se deplacer en haut vers la droite 'ru'
+        if( d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()-1] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()-1] != '#')
+
+            d.deplaceRightUp(d_aventurier);
+        break;
+    case 7 ://se deplacer en bas vers la gauche 'ld'
+          if( d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()+1] != 'x'
+            && d_terrain.tableau()[d_aventurier->position().x()-1][d_aventurier->position().y()+1] != '#')
+
+            d.deplaceLeftDown(d_aventurier);
+         break;
+    case 8 :// se daplacer en bas vers la droite 'rd'
+        if( d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()+1] != 'x'
+          && d_terrain.tableau()[d_aventurier->position().x()+1][d_aventurier->position().y()+1] != '#')
+
+            d.deplaceRightDown(d_aventurier);
+         break;
     }
+
 }
-
-
